83b8500c0d5627bc842fec9f40ec067151e4cda6
Scott Schoeller <23561648+sschoellerSTEM@users.noreply.github.com>
2019-01-28 10:53:12 -0600

    Update uniq_PDB.R
    Changed how filename was entered into R due to limitations in interactive vs script mode; reformatted comment block

--- uniq_PDB.R
+++ uniq_PDB.R
@@ -1,12 +1,15 @@
 # Programmer: Scott Schoeller (sschoellerSTEM)
 # Purpose: Counts PDB Identifiers with Chain A out of a CSV file
 # Note: May need to replace spaces with underscores in CSV header names
-# Dates: 26 Jan 2019, 27 Jan 2019
+# Creation Date: 26 Jan 2019
+# Revisions: 27 Jan 2019, 28 Jan 2019
 
-f<-readline("Enter a CSV file: ")
-if (f == "") 
-	f<-"tabularResults.csv" # default Protein Data Bank CSV
- 
+
+f<-"" # change the value of f if not tabularResults.csv 
+
+if (f == "")
+  f<-"tabularResults.csv" # default Protein Data Bank CSV
+
 csv<-read.table(file=f, header=TRUE, sep=",")
 
 uniqueID<-csv$PDB_ID[csv$Chain_ID == "A"]
@@ -14,7 +17,7 @@ uniqueID<-csv$PDB_ID[csv$Chain_ID == "A"]
 num<-0 # counter
 
 for (item in uniqueID) {
-	print(item)
+	#print(item) # typically for debugging
 	num<-num+1
 }
 
